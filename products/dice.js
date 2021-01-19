937afd9f81bf6351573dfe302f397e71969e369b
Julianne Vela <cjvela@outlook.com>
2021-01-19 14:46:40 -0800

    Created raw data array of all products and exported to render-function.js

---
+++ products/dice.js
@@ -0,0 +1,132 @@
+export const productsArr = [
+    {
+        id: 1,
+        name: 'RPG Set - Purple Moonstone',
+        image: 'p-dice-purple.jpg',
+        description: 'A set of 7 Polymer RPG dice including a d3, d4, d6, d8, d10, d12 and d20.',
+        category: 'polymer-dice',
+        price: 9.95
+    },
+    {
+        id: 2,
+        name: 'RPG Set - Pride',
+        image: 'p-dice-pride.png',
+        description: 'A special Pride edition set of 7 RPG dice including a d3, d4, d6, d8, d10, d12 and d20. Proceeds go to support LGBTQ+ advocacy groups!',
+        category: 'polymer-dice',
+        price: 6.95
+    },
+    {
+        id: 3,
+        name: 'RPG Set - Andromeda Galaxy',
+        image: 'p-dice-galaxy.jpg',
+        description: 'A set of 7 Polymer RPG dice including a d3, d4, d6, d8, d10, d12 and d20.',
+        category: 'polymer-dice',
+        price: 9.95
+    },
+    {
+        id: 4,
+        name: 'RPG Set - Crimson Embers',
+        image: 'p-dice-crimson.png',
+        description: 'A set of 7 Polymer RPG dice including a d3, d4, d6, d8, d10, d12 and d20.',
+        category: 'polymer-dice',
+        price: 9.95
+    },
+    {
+        id: 5,
+        name: 'RPG Set - Antique Bone',
+        image: 'p-dice-bone.png',
+        description: 'A set of 7 Polymer RPG dice including a d3, d4, d6, d8, d10, d12 and d20.',
+        category: 'polymer-dice',
+        price: 9.95
+    },
+    {
+        id: 6,
+        name: 'RPG Set - Golden Aqua',
+        image: 'm-dice-aqua.jpg',
+        description: 'A set of 7 Metal RPG dice including a d3, d4, d6, d8, d10, d12 and d20.',
+        category: 'metal-dice',
+        price: 11.95
+    },
+    {
+        id: 7,
+        name: 'RPG Set - Sinister Emerald',
+        image: 'm-dice-emerald.jpg',
+        description: 'A set of 7 Metal RPG dice including a d3, d4, d6, d8, d10, d12 and d20.',
+        category: 'metal-dice',
+        price: 11.95
+    },
+    {
+        id: 8,
+        name: 'RPG Set - Deadly Nightshade',
+        image: 'm-dice-nightshade.jpg',
+        description: 'A set of 7 Metal RPG dice including a d3, d4, d6, d8, d10, d12 and d20.',
+        category: 'metal-dice',
+        price: 11.95
+    },
+    {
+        id: 9,
+        name: 'RPG Set - Ominous Nightfall',
+        image: 'm-dice-nightfall.jpg',
+        description: 'A set of 7 Metal RPG dice including a d3, d4, d6, d8, d10, d12 and d20.',
+        category: 'metal-dice',
+        price: 11.95
+    },
+    {
+        id: 10,
+        name: 'RPG Set - Rising Phoenix',
+        image: 'm-dice-phoenix.jpg',
+        description: 'A set of 7 Metal RPG dice including a d3, d4, d6, d8, d10, d12 and d20.',
+        category: 'metal-dice',
+        price: 11.95
+    },
+    {
+        id: 11,
+        name: 'Magnetic Dice Tray - Black',
+        image: 'mag-rect-tray-black.png',
+        description: 'A magnetic rectangular dice tray with a soft black felt lining.',
+        category: 'containers',
+        price: 14.95
+    },
+    {
+        id: 12,
+        name: 'Magnetic Dice Tray - Teal',
+        image: 'mag-rect-tray-teal.jpg',
+        description: 'A magnetic rectangular dice tray with a soft teal felt lining.',
+        category: 'containers',
+        price: 14.95
+    },
+    {
+        id: 13,
+        name: 'Magnetic Dice Tray - Green',
+        image: 'mag-rect-tray-green.png',
+        description: 'A magnetic rectangular dice tray with a soft green felt lining.',
+        category: 'containers',
+        price: 14.95
+    },
+    {
+        id: 14,
+        name: 'Hard Metal Case',
+        image: 'metal-case.jpg',
+        description: 'A hard metal dice case that is excellent for storing your dice on-the-go!',
+        category: 'containers',
+        price: 14.95
+    },
+    {
+        id: 15,
+        name: 'Medium Dice Bag',
+        image: 'med-bag.jpg',
+        description: 'A nice medium sized dice bag to hold all your lucky magic balls of fun!',
+        category: 'containers',
+        price: 14.95
+    },
+    {
+        id: 16,
+        name: 'Mystery Bag',
+        image: 'mystery-bag.jpg',
+        description: 'Oh, how I\'ve piqued your curiosity! Wait...you\'re not a cat, right?',
+        category: 'containers',
+        price: 14.95
+    },
+];
+
+console.log(productsArr);
