c252fcb32cf84881939017b7f6dc9780d3a96248
Julianne Vela <cjvela@outlook.com>
2021-01-20 15:23:07 -0800

    added dummy-data for cart items and exported array cartData

---
+++ data/cart-data.js
@@ -0,0 +1,18 @@
+export const cartData = [
+    {
+        id: 2,
+        quantity: 2,
+    },
+    {
+        id: 3,
+        quantity: 1,
+    },
+    {
+        id: 15,
+        quantity: 3,
+    },
+    {
+        id: 16,
+        quantity: 1,
+    },
+];
