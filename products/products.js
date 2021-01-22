489f1385e22b58e2f8749c1f135f1ec8a2995304
Julianne Vela <cjvela@outlook.com>
2021-01-21 16:40:43 -0800

    changed links to buttons for checkout and home on products page

--- products/products.js
+++ products/products.js
@@ -2,6 +2,8 @@ import { productsArr } from '../data/product-data.js';
 import { renderProduct } from '../utils.js';
 
 const list = document.getElementById('product-list');
+const checkoutButton = document.querySelector('.checkout');
+const homeButton = document.querySelector('.home');
 
 for (let product of productsArr) {
     const productElement = renderProduct(product);
@@ -9,4 +11,9 @@ for (let product of productsArr) {
     list.append(productElement);
 }
 
-// event listener is within renderProduct function
+checkoutButton.addEventListener('click', () => {
+    location.href = '../cart/index.html';
+});
+homeButton.addEventListener('click', () => {
+    location.href = '../index.html';
+});
